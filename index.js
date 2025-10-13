eeec191833d32e644b053ef43011ca4992caca2a
Yankey Ange <nwolle14@gmail.com>
2025-10-13 01:13:42 +0000

    second changes

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
-const bouton = document.querySelector('button');
+// déclaration de la variable
+
+var bouton = document.getElementById('buton1');
+
+// l'évènement au click
 
 bouton.addEventListener('click', function() {
-    //
-    if (bouton.innerText === "Voir Plus") {
-        bouton.innerText === ""
+    
+    if(bouton.innerText === "VOIR PLUS") {
 
-} else {
+        bouton.innerText = "voir moins";
+        bouton.style.backgroundColor = "green";
 
-}
+    } else {
+        bouton.innerText = "VOIR PLUS";
+        // il reprend sa couleur d'origine
+        bouton.style.backgroundColor = "";
+    }
+   
+});
+
+// déclaration de la variable pour le bouton 2
+
+var bouton2 = document.getElementById('buton2');
+
+// l'évènement au click
+
+bouton2.addEventListener('click', function() {
+    
+    if(bouton2.innerText === "VOIR PLUS") {
+
+        bouton2.innerText = "voir moins";
+        bouton2.style.backgroundColor = "red";
+
+    } else {
+        bouton2.innerText = "VOIR PLUS";
+        // il reprend sa couleur d'origine
+        bouton2.style.backgroundColor = "";
+    }
+   
+});
+
+// déclaration de la variable pour le bouton 3
+
+var bouton3 = document.getElementById('buton3');
+
+// l'évènement au click
+
+bouton3.addEventListener('click', function() {
+    
+    if(bouton3.innerText === "VOIR PLUS") {
+
+        bouton3.innerText = "voir moins";
+        bouton3.style.backgroundColor = "white";
+        bouton3.style.color = "black";
+
+    } else {
+        bouton3.innerText = "VOIR PLUS";
+        // il reprend sa couleur d'origine
+        bouton3.style.backgroundColor = "";
+    }
    
 });
