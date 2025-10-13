1d787c9c36799c2c9701f456d973734ae748f552
Yankey Ange <nwolle14@gmail.com>
2025-10-13 00:24:52 +0000

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const bouton = document.querySelector('button');
+
+bouton.addEventListener('click', function() {
+    //
+    if (bouton.innerText === "Voir Plus") {
+        bouton.innerText === ""
+
+} else {
+
+}
+   
+});
